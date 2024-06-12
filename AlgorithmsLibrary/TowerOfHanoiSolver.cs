da05feb68bce8fbb89cdf4540596a5d1889121b3
Wiktor Kalaga <wkalaga73@gmail.com>
2024-06-12 23:24:32 +0200

    Added visualization to project. Configured as WPF

--- AlgorithmsLibrary/TowerOfHanoiSolver.cs
+++ AlgorithmsLibrary/TowerOfHanoiSolver.cs
@@ -1,7 +1,26 @@
 ﻿namespace AlgorithmsLibrary
 {
-    public class TowerOfHanoiSolver
+    public static class TowerOfHanoiSolver
     {
+        public static (int, List<string>) Recursion(int n, string from, string to, string via)
+        {
+            int numberOfMoves = 0;
+            List<string> moves = [];
 
+            if (n is 0)
+            {
+                numberOfMoves++;
+                moves.Add($"Przenieś krążek 1 z wieży: [{from}] na wieże: [{to}]. Ruch: [{numberOfMoves}]");
+            }
+            else
+            {
+                Recursion(n - 1, from, via, to);
+                numberOfMoves++;
+                moves.Add($"Przenieś krążek {n} z wieży: [{from}] na wieże: [{to}]. Ruch: [{numberOfMoves}]");
+                Recursion(n - 1, via, to, from);
+            }
+
+            return (numberOfMoves, moves);
+        }
     }
 }
